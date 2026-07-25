861252176380fa946940c594a5c74200b4246fae
Tharindu Kalhara <tharindukalhara73@gmail.com>
2026-07-25 20:07:01 +0530

    Instrument Django app with django-prometheus for /metrics endpoint
    Required for the new ServiceMonitor (k8s-manifests/svc-monitor.yaml) to
    have anything to scrape. Also normalized requirements.txt to UTF-8 while
    editing it (was UTF-16, a pre-existing quirk noted in SETUP_GUIDE.md;
    functionally no different, just easier to edit going forward).

--- demo-application/simply/settings.py
+++ demo-application/simply/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'django_prometheus',
 ]
 
 MIDDLEWARE = [
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'simply.urls'
